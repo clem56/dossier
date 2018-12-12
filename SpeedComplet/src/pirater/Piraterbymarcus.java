f6cbb7b7e769d8c33d4d2acb30fa1001dcb91fdf
admin <admin@admin-PC>
2018-12-12 14:02:37 +0100

    un petit coucou les amis

--- SpeedComplet/src/pirater/Piraterbymarcus.java
+++ SpeedComplet/src/pirater/Piraterbymarcus.java
@@ -12,16 +12,13 @@ public class Piraterbymarcus {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-<<<<<<< HEAD
-
 
 	@Override
 	public String toString() {
 		return "Piraterbymarcus [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
 				+ super.toString() + "]";
 	}
-=======
->>>>>>> branch 'master' of https://github.com/clem56/dossier.git
-	
+
+	//COUCOU LES AMIS
 	
 }
